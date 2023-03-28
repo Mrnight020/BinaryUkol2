2ee8ef40088b2b7534d8446c33879571b8413881
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-03-28 19:52:04 +0200

    Add files via upload

---
+++ Ukol2/Ukol2Binarni/Ukol2Binarni/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Ukol2Binarni
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(37, 129);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 156);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zobraz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(349, 97);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 249);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(653, 13);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 359);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ukol2/Ukol2Binarni/Ukol2Binarni/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ukol2Binarni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            FileStream data = new FileStream("sport.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter zapisuj = new BinaryWriter(data, Encoding.GetEncoding("windows-1250"));
+            BinaryReader ctibinary = new BinaryReader(data, Encoding.GetEncoding("windows-1250"));
+            StreamReader cti = new StreamReader("sport.txt", Encoding.GetEncoding("windows-1250"));
+
+            while(!cti.EndOfStream)
+            {
+                string radek = cti.ReadLine();
+                string[] polozky = radek.Split(';');
+
+                zapisuj.Write(int.Parse(polozky[0]));
+                zapisuj.Write(polozky[1]);
+                zapisuj.Write(polozky[2]);
+                zapisuj.Write(char.Parse(polozky[3]));
+                zapisuj.Write(int.Parse(polozky[4]));
+                zapisuj.Write(int.Parse(polozky[5]));
+
+            }
+
+            ctibinary.BaseStream.Position = 0;
+            while(ctibinary.BaseStream.Position < ctibinary.BaseStream.Length)
+            {
+                string celyradek = "" + ctibinary.ReadInt32() + ";"
+                                      + ctibinary.ReadString() + ";" 
+                                      + ctibinary.ReadString() + ";" 
+                                      + ctibinary.ReadChar() + ";" 
+                                      + ctibinary.ReadInt32() + ";" 
+                                      + ctibinary.ReadInt32();
+
+                textBox1.AppendText(celyradek + Environment.NewLine);
+                
+            }
+
+
+            data.Close();
+            cti.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
